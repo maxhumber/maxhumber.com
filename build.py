711a5a9cb10ed4c5f9b241b481096488bc12b6ae
Max Humber <max.humber@gmail.com>
2019-09-19 14:24:39 -0400

    massive refactor

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import shutil
-import subprocess
+from subprocess import call
 from fire import Fire
 from jinja2 import Environment, FileSystemLoader
 from markdown2 import markdown, markdown_path
@@ -17,61 +17,58 @@ BLOG = Path('blog')
 PAGES = Path('pages')
 OUT = Path('output')
 
-# TODO: refactor this into two parts
-def jupyter_to_container(path):
-    '''Convert a Jupyter Notebook into a Markdown Container'''
-    with open(path, 'r', encoding='utf-8') as f:
-        notebook = nbformat.reads(f.read(), as_version=4)
+def extract_notebook_metadata(notebook):
+    '''Extract metadata from the first Jupyter Notebook cell'''
     metadata = notebook['cells'].pop(0)
     metadata = metadata['source'].split('\n')[1:-1]
     metadata = [m.split(':') for m in metadata]
-    container = {m[0]:m[1].strip() for m in metadata}
-    md_exporter = MarkdownExporter()
-    md, resources = md_exporter.from_notebook_node(notebook)
+    return {m[0]:m[1].strip() for m in metadata}
+
+def convert_notebook(path):
+    '''Convert a Jupyter Notebook into Markdown and a Container'''
+    with open(path, 'r', encoding='utf-8') as f:
+        notebook = nbformat.reads(f.read(), as_version=4)
+    container = extract_notebook_metadata(notebook)
+    md, resources = MarkdownExporter().from_notebook_node(notebook)
     for fn, bytes in resources['outputs'].items():
         with open(f"{BLOG}/images/{container['slug']}-{fn}", 'wb') as f:
             f.write(bytes)
     md = md.replace('![png](', f"![png](images/{container['slug']}-")
-    html = markdown(md, extras=['fenced-code-blocks'])
-    template = JIN.get_template('blog_post.html')
-    container['content'] = template.render(html=html)
-    return container
+    return md, container
 
-def build_pages():
-    '''Build, render, and write pages to the output folder'''
-    template = JIN.get_template('page.html')
-    for p in PAGES.glob('*.md'):
-        html = template.render(html=markdown_path(p))
-        file = OUT / p.name.replace('.md', '.html')
-        with file.open('w', encoding='utf-8') as f:
-            f.write(html)
-
-# TODO: simplify the for loops
 def build_blog_posts():
-    '''Build, render, and write blog posts to the output folder'''
-    posts = []
+    '''Build and render all the blog posts'''
     template = JIN.get_template('blog_post.html')
-    for b in BLOG.glob('*.md'):
-        html = markdown_path(b, extras=['metadata', 'fenced-code-blocks'])
-        container = html.metadata.copy()
-        container['content'] = template.render(html=str(html))
+    posts = []
+    for blog_post in BLOG.iterdir():
+        if blog_post.name.endswith('.md'):
+            html = markdown_path(blog_post, extras=['metadata', 'fenced-code-blocks'])
+            container = html.metadata.copy()
+        elif blog_post.name.endswith('.ipynb'):
+            md, container = convert_notebook(blog_post)
+            html = markdown(md, extras=['fenced-code-blocks'])
+        else:
+            continue
+        container['content'] = template.render(html=html)
         posts.append(container)
-    for j in BLOG.glob('*.ipynb'):
-        container = jupyter_to_container(j)
-        posts.append(container)
-    for p in posts:
-        file = OUT / f"{p['slug']}.html"
-        with file.open('w', encoding='utf-8') as f:
-            f.write(p['content'])
     return posts
 
 def build_blog_index(posts):
     '''Build the index for all the blog posts'''
     template = JIN.get_template('blog_index.html')
-    index = template.render(posts=posts)
-    file = OUT / 'blog.html'
-    with file.open('w', encoding='utf-8') as f:
-        f.write(index)
+    html = template.render(posts=posts)
+    container = {'slug': 'blog', 'content': html}
+    return [container]
+
+def build_pages():
+    '''Build and render all the additional pages'''
+    template = JIN.get_template('page.html')
+    pages = []
+    for p in PAGES.glob('*.md'):
+        html = template.render(html=markdown_path(p))
+        container = {'slug': p.name.replace('.md', ''), 'content': html}
+        pages.append(container)
+    return pages
 
 def build():
     '''Actually build the entire blog'''
@@ -80,33 +77,31 @@ def build():
     except FileNotFoundError:
         pass
     OUT.mkdir(parents=True, exist_ok=True)
-    build_pages()
-    build_blog_index(build_blog_posts())
+    posts = build_blog_posts()
+    blog_index = build_blog_index(posts)
+    pages = build_pages()
+    everything = posts + blog_index + pages
+    for page in everything:
+        file = OUT / f"{page['slug']}.html"
+        with file.open('w', encoding='utf-8') as f:
+            f.write(page['content'])
     shutil.copyfile('CNAME', OUT / 'CNAME')
     shutil.copyfile('favicon.ico', OUT / 'favicon.ico')
     shutil.copytree(BLOG / 'images', OUT / 'images')
     shutil.copytree('static', OUT / 'static')
 
-def shell(command):
-    '''Execute bash commands'''
-    if isinstance(command, str):
-        command = [command]
-    [subprocess.call([c], shell=True) for c in command]
-
 def preview():
     '''Preview static website'''
     JIN.globals['DEVELOPMENT'] = True
     build()
-    shell('cd output; python -m http.server')
+    call('cd output; python -m http.server', shell=True)
 
 def publish():
     '''Push static website to GitHub Pages'''
     build()
-    shell([
-        f'git add {str(OUT)}',
-        'git commit -m "new blog post"',
-        'git push origin `git subtree split --prefix output master`:gh-pages --force'
-    ])
+    call(f'git add {str(OUT)}', shell=True)
+    call('git commit -m "new blog post"', shell=True)
+    call('git push origin `git subtree split --prefix output master`:gh-pages --force', shell=True)
 
 if __name__ == '__main__':
     Fire({
@@ -115,7 +110,6 @@ if __name__ == '__main__':
     })
 
 # TODO:
-# refactor the jupyter and build posts functions
 # fix /blog.html vs /blog on local
 # rss feeds for python tags
 # blog aggregator (eventually)
