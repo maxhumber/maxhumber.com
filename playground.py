8b75c51ce16c6fcdb333b51aac19be9b6dcab476
Max Humber <max.humber@gmail.com>
2019-09-30 22:48:38 -0400

    notebook html fixes

--- playground.py
+++ playground.py
@@ -1,68 +1,23 @@
-import mistune
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name
-from pygments.formatters.html import HtmlFormatter
+import re
 
-with open('blog/2019-09-23_mummify.md', 'r') as f:
-    md = f.read()
+with open('output/yass.html', 'r') as f:
+    html = f.read()
 
-##### meta metadata
+html = re.sub('\n</table>\n</div>', '\n</table>\n</div>\n<br/>\n', html)
 
-def extract_metadata(md):
-    dash_splits = md.split('---', maxsplit=2)
-    meta = dash_splits[1]
-    meta = meta.split('\n')[1:-1]
-    meta = [m.split(': ', 1) for m in meta]
-    meta = {k.strip(): v.strip() for k, v in meta}
-    body = fence_splits[2]
-    return meta, body
+def fix_notebook_html(html):
+    # add break line after code
+    html = re.sub('\n</pre></div>','\n</pre></div>\n<br/>\n', html)
+    # fix code outputs
+    html = re.sub('\n\n\n</code></pre>','</code></pre>\n<br/>', html)
+    # remove table style
+    html = re.sub(r'\n<style scoped>.*?</style>', '', html, 0, flags=re.DOTALL)
+    # remove table border
+    html = re.sub('<table border="1"', '<table', html)
+    # add break line after table
+    html = re.sub('\n</table>\n</div>', '\n</table>\n</div>\n<br/>\n', html)
+    return html
 
-extract_metadata(md)
-
-
-
-####
-
-
-class HighlightRenderer(mistune.Renderer):
-    def block_code(self, code, lang):
-        if not lang:
-            return f'\n<pre><code>{mistune.escape(code)}</code></pre>\n'
-        lexer = get_lexer_by_name(lang, stripall=True)
-        formatter = HtmlFormatter()
-        return highlight(code, lexer, formatter)
-
-renderer = HighlightRenderer()
-markdown = mistune.Markdown(renderer=renderer)
-html = markdown(md)
 
 with open('output/test.html', 'w') as f:
     f.write(html)
-
-
-
-
-table(header, body)
-
-    Rendering table element. Wrap header and body in it.
-    Parameters:
-
-        header – header part of the table.
-        body – body part of the table.
-
-table_cell(content, **flags)
-
-    Rendering a table cell. Like <th> <td>.
-    Parameters:
-
-        content – content of current table cell.
-        header – whether this is header or not.
-        align – align of current table cell.
-
-table_row(content)
-
-    Rendering a table row. Like <tr>.
-    Parameters:	content – content of current table row.
-
-
-##
